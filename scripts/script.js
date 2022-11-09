03da9ac84e3a9868b4de2146f432b55d6e660e47
k-fields <caiocp3@hotmail.com>
2022-11-09 01:59:40 -0300

    Switching computer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
-for (let i = 0; i < 5; i++){
-    const playerSelection = prompt("Choose: rock, paper or scissors.");
-    const computerSelection = getComputerChoice();
+function game(){
+    for (let i = 0; i < 5; i++){
+        const playerSelection = prompt("Choose: rock, paper or scissors.");
+        const computerSelection = getComputerChoice();
 
-    
+        return "Game "+i+": you " + evaluate(playerSelection, computerSelection);
+    }
+}
+
+function getComputerChoice(){
+
+}
+
+function evaluate(player, computer){
+    let won_or_lost = '';
+
+    if (player === 'rock'){
+
+    }
+    else if(player === 'paper'){
+
+    }
+    else if (player === 'scissors'){
+        
+    }
+
+
+    return won_or_lost;
 }
