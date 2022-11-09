9d6a361b9c837f39693143622ab1345bb9189a11
k-fields <caiocp3@hotmail.com>
2022-11-09 03:39:23 -0300

    Functional game() function

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,56 @@
 function game(){
-    for (let i = 0; i < 5; i++){
+    for (let i = 1; i <= 5; i++){
         const playerSelection = prompt("Choose: rock, paper or scissors.");
         const computerSelection = getComputerChoice();
 
-        return "Game "+i+": you " + evaluate(playerSelection, computerSelection);
+        console.log("Game "+i+": you " + evaluate(playerSelection, computerSelection));
     }
 }
 
 function getComputerChoice(){
+    rand = Math.floor(Math.random() * 3);
+    let choice = '';
 
+    switch(rand){
+        case 0:
+            choice = 'rock';
+            break;
+        case 1:
+            choice = 'paper';
+            break;
+        case 2:
+            choice = 'scissors';
+            break;
+        default:
+            choice = 'error';
+            break;
+    }
+    return rand;
 }
 
 function evaluate(player, computer){
     let won_or_lost = '';
 
     if (player === 'rock'){
-
+        if (computer === 'rock') won_or_lost = 'got a tie';
+        if (computer === 'paper') won_or_lost = 'lost';
+        else won_or_lost = 'won';
     }
     else if(player === 'paper'){
-
+        if (computer === 'rock') won_or_lost = 'won';
+        if (computer === 'paper') won_or_lost = 'got a tie';
+        else won_or_lost = 'lost';
     }
     else if (player === 'scissors'){
-        
+        if (computer === 'rock') won_or_lost = 'lost';
+        if (computer === 'paper') won_or_lost = 'won';
+        else won_or_lost = 'got a tie';
     }
-
+    else{
+        won_or_lost = 'Choose a valid option: "rock", "paper" or "scissors".';
+    }
 
     return won_or_lost;
 }
+
+console.log(game());
